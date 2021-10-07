8b718693f18454207e3523e74e50eefd7a9547e2
Paweł Kocielny <pawel.koscielny2904@gmail.com>
2021-10-07 17:49:41 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useTabBlurFocus } from "./Hooks";
+import { WebSockets } from "./WebSocket/WebSockets";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const [state, setState] = useState({});
+  // const func = (arg: Arg) => {
+  //   if (arg.name === "CLS") console.log(arg);
+  //   else console.log(arg);
+  // };
+  const tabState = useTabBlurFocus();
+  useEffect(() => {
+    // console.log(tabState);
+  }, [tabState]);
+  // reportWebVitals(func);
+  return <WebSockets />;
 }
 
 export default App;
